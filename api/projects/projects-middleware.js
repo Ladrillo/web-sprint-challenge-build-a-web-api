9038aff75df6b44b792899f3b03c75a4fd9f1475
Kelly Jerrell <kcjerrell@gmail.com>
2021-07-09 08:26:33 -0700

    server bones, project get and post

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,24 @@
 // add middlewares here related to projects
+
+/** @type {import("express").RequestHandler} */
+const validateProject = (req, res, next) => {
+	if (req.body) {
+		const { name, description, completed } = req.body;
+
+		if (name && name.length > 0 && description && description.length > 0) {
+			req.newPost = {
+				name,
+				description,
+				completed: Boolean(completed)
+			};
+			next();
+		}
+		else {
+			res.status(400).send("bad request");
+		}
+	}
+};
+
+module.exports = {
+	validateProject,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,43 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const projects = require(`./projects-model`);
+const middleware = require('./projects-middleware');
+const { validateProject } = middleware;
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	try {
+		const result = await projects.get();
+		res.status(200).json(result ?? []);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	try {
+		const result = await projects.get(req.params.id);
+		if (result)
+			res.status(200).json(result);
+		else
+			res.status(404).send("Not found!");
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+router.post('/', validateProject, async (req, res) => {
+	try {
+		const result = await projects.insert(req.newPost);
+		if (result)
+			res.status(201).json(result);
+		else
+			res.status(400).send("Unknown fail");
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+module.exports = router;
