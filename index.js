e0ba57c70865b243617c1025af0784106fb22e20
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-13 15:11:37 +0200

    take out dotenv

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
-require('dotenv').config();
+// require('dotenv').config();
 
 const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => { console.log(`listening on port ${PORT}`) });
-
