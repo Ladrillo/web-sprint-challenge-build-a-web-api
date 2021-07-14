f7fd1c10311eef0b4bee562ecbab3b23678d229b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-14 14:49:27 +0200

    veamos

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,7 @@
 // Write your "projects" router here!
 const express = require('express');
 
-const projects = require(`./projects-model`);
+const projects = require('./projects-model');
 const { validateProject, validateId } = require('./projects-middleware');
 
 const router = express.Router();
