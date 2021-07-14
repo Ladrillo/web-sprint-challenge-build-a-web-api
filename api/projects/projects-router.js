b92fb0d268857a3d8496b1d6616d7f2ef2331660
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-14 14:56:12 +0200

    disconnect endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -6,58 +6,58 @@ const projects = require('./projects-model');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-	try {
-		const result = await projects.get();
-		res.status(200).json(result ?? []);
-	} catch (err) {
-		next(err)
-	}
-});
+// router.get('/', async (req, res, next) => {
+// 	try {
+// 		const result = await projects.get();
+// 		res.status(200).json(result ?? []);
+// 	} catch (err) {
+// 		next(err)
+// 	}
+// });
 
-router.get('/:id',  (req, res) => {
-	res.status(200).json(req.project);
-});
+// router.get('/:id',  (req, res) => {
+// 	res.status(200).json(req.project);
+// });
 
-router.post('/',  async (req, res, next) => {
-	try {
-		const result = await projects.insert(req.project);
-		if (result)
-			res.status(201).json(result);
-		else
-			res.status(400).send("Unknown fail");
-	} catch (err) {
-		next(err);
-	}
-});
+// router.post('/',  async (req, res, next) => {
+// 	try {
+// 		const result = await projects.insert(req.project);
+// 		if (result)
+// 			res.status(201).json(result);
+// 		else
+// 			res.status(400).send("Unknown fail");
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
 
-router.put('/:id',  async (req, res, next) => {
-	try {
-		const result = await projects.update(req.params.id, req.project);
-		res.status(200).json(result);
-	} catch (err) {
-		next(err);
-	}
-});
+// router.put('/:id',  async (req, res, next) => {
+// 	try {
+// 		const result = await projects.update(req.params.id, req.project);
+// 		res.status(200).json(result);
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
 
-router.delete('/:id', (req, res, next) => {
-	projects.remove(req.projectId)
-		.then(result => {
-			if (result)
-				res.status(200).send();
-			else
-				res.status(400).message("could not be deleted");
-		}, err => next(err));
-});
+// router.delete('/:id', (req, res, next) => {
+// 	projects.remove(req.projectId)
+// 		.then(result => {
+// 			if (result)
+// 				res.status(200).send();
+// 			else
+// 				res.status(400).message("could not be deleted");
+// 		}, err => next(err));
+// });
 
-router.get('/:id/actions',  async (req, res, next) => {
-	try {
-		const result = await projects.getProjectActions(req.projectId);
-		res.status(200).json(result);
-	} catch (err) {
-		next(err);
-	}
-});
+// router.get('/:id/actions',  async (req, res, next) => {
+// 	try {
+// 		const result = await projects.getProjectActions(req.projectId);
+// 		res.status(200).json(result);
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
 
 // eslint-disable-next-line no-unused-vars
 router.use((err, req, res, next) => {
